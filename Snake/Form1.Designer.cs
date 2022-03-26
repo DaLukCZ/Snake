9cc1541e342c9cf1127aac712e20bff7a9327859
DaLuk <lukas.smach@seznam.cz>
2022-03-26 16:23:47 +0100

    Snake 1.0
    Without top score

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.food = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(11, 15);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(98, 21);
+            this.labelScore.TabIndex = 0;
+            this.labelScore.Text = "Score: 0";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.labelTime);
+            this.panel1.Controls.Add(this.labelScore);
+            this.panel1.Location = new System.Drawing.Point(0, 800);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 50);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Best Score: 100";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(631, 15);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(109, 21);
+            this.labelTime.TabIndex = 1;
+            this.labelTime.Text = "Time: 0 s";
+            // 
+            // food
+            // 
+            this.food.BackgroundImage = global::Snake.Properties.Resources.jabko;
+            this.food.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.food.Location = new System.Drawing.Point(381, 315);
+            this.food.Name = "food";
+            this.food.Size = new System.Drawing.Size(20, 20);
+            this.food.TabIndex = 3;
+            this.food.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 850);
+            this.Controls.Add(this.food);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.PictureBox food;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        int cols = 40;
+        int rows = 40;
+        int dx = 0;
+        int dy = 0;
+        int front = 0;
+        int back = 0;
+        int score = 0;
+        int seconds = 0;
+        bool right = true;
+        bool left = true;
+        bool up = true;
+        bool down = true;
+        Pixel[] snake = new Pixel[1600];
+        List<int> free = new List<int>();
+        bool[,] visit;
+        Random random = new Random();
+        System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+            startTimer();
+        }
+
+        private void startTimer()
+        {
+            timer1.Interval = 100;
+            timer1.Tick += move;
+            timer1.Start();
+        }
+
+        private void move(object sender, EventArgs e)
+        {
+            int x = snake[front].Location.X;
+            int y = snake[front].Location.Y;
+            if (dx == 0 && dy == 0)
+            {
+                return;
+            }
+            if (gameOver(x + dx, y + dy))
+            {
+                timer1.Stop();
+                MessageBox.Show("Game Over");
+                return;
+            }
+            if (collisionFood(x + dx, y + dy))
+            {
+                score++;
+                labelScore.Text = "Score: " + score;
+                if (hits((y + dy) / 20, (x + dx) / 20))
+                {
+                    return;
+                }
+                Pixel head = new Pixel(x + dx, y + dy);
+                front = (front - 1 + 1600) % 1600;
+                snake[front] = head;
+                visit[head.Location.Y / 20, head.Location.X / 20] = true;
+                Controls.Add(head);
+                randomFood();
+            }
+            else
+            {
+                if (hits((y + dy) / 20, (x + dx) / 20))
+                {
+                    return;
+                }
+                visit[snake[back].Location.Y / 20, snake[back].Location.X / 20] = false;
+                front = (front - 1 + 1600) % 1600;
+                snake[front] = snake[back];
+                snake[front].Location = new Point(x + dx, y + dy);
+                back = (back - 1 + 1600) % 1600;
+                visit[(y + dy) / 20, (x + dx) / 20] = true;
+            }
+        }
+
+        private void randomFood()
+        {
+            free.Clear();
+            if (!win())
+            {
+                for (int x = 0; x < rows; x++)
+                {
+                    for (int y = 0; y < cols; y++)
+                    {
+                        if (!visit[x, y])
+                        {
+                            free.Add(x * cols + y);
+                        }
+                    }
+                }
+                food.Location = new Point((random.Next() % cols) * 20, (random.Next() % rows) * 20);
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("vyhrál jsi lol");
+            }
+        }
+
+        private bool win()
+        {
+            if(score == 1600)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private bool hits(int x, int y)
+        {
+            if (visit[x, y])
+            {
+                timer1.Stop();
+                MessageBox.Show("Snake hit his body");
+                return true;
+            }
+            return false;
+        }
+
+        private bool collisionFood(int x, int y)
+        {
+            return x == food.Location.X && y == food.Location.Y; 
+        }
+
+        private bool gameOver(int x, int y)
+        {
+            return x < 0 || y < 0 || x > 799 || y > 799;
+        }
+
+        private void init()
+        {
+            visit = new bool[rows, cols];
+            Pixel head = new Pixel((random.Next() % cols) * 20, (random.Next() % rows) * 20);
+            food.Location = new Point((random.Next() % cols) * 20, (random.Next() % rows) * 20);
+
+            for(int x = 0; x < rows; x++)
+            {
+                for (int y = 0; y < cols; y++)
+                {
+                    visit[x, y] = false;
+                    free.Add(x * cols + y);
+                }
+                visit[head.Location.Y / 20, head.Location.X / 20] = true;
+                free.Remove(head.Location.Y / 20 * cols + head.Location.X / 20);
+                Controls.Add(head);
+                snake[front] = head;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            timer2.Start();
+            dx = dy = 0;
+            switch (e.KeyCode)
+            {
+
+                case Keys.Right:
+                    if (right)
+                    {
+                        left = false;
+                        up = true;
+                        down = true;
+                        dx = 20;
+                    }
+                    else
+                    {
+                        dx = -20;
+                    }
+                    break;
+                case Keys.Left:
+                    if (left)
+                    {
+                        right = false;
+                        up = true;
+                        down = true;
+                        dx = -20;
+                    }
+                    else
+                    {
+                        dx = 20;
+                    }
+                    break;
+                case Keys.Up:
+                    if (up)
+                    {
+                        down = false;
+                        right = true;
+                        left = true;
+                        dy = -20;
+                    }
+                    else
+                    {
+                        dy = 20;
+                    }
+                    break;
+                case Keys.Down:
+                    if (down)
+                    {
+                        up = false;
+                        right = true;
+                        left = true;
+                        dy = 20;
+                    }
+                    else
+                    {
+                        dy = -20;
+                    }
+                    break;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = "Time: " + seconds++ + " s";
+        }
+    }
+}
